f39fc4c9121eb861d0fc96c59d3465d71446228b
Ervy Yabut <ervy19@gmail.com>
2014-11-29 15:18:19 +0800

    Create resource routes for views

--- app/routes.php
+++ app/routes.php
@@ -5,13 +5,26 @@
 | Application Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
+| Different application routes of TEAMS
 |
 */
 
 Route::get('/', function()
 {
-	return View::make('hello');
+	return View::make('index');
 });
+
+Route::resource('employees','EmployeesController');
+
+Route::resource('campuses','CampusesController');
+
+Route::resource('departments','DepartmentsController');
+
+Route::resource('schools_colleges','SchoolsCollegesController');
+
+Route::resource('internal_trainings','InternalTrainingsController');
+
+Route::resource('external_trainings','ExternalTrainingsController');
+
+Route::resource('skills_competencies','SkillsCompetenciesController');
+
