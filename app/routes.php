cdd9d7d3ff484b68ad119b51c34ff9a4572942c7
Alex Angeles <alexismayangeles@gmail.com>
2014-12-01 16:49:26 +0800

    Add positions path in routes.php

--- app/routes.php
+++ app/routes.php
@@ -20,6 +20,8 @@ Route::resource('campuses','CampusesController');
 
 Route::resource('departments','DepartmentsController');
 
+Route::resource('positions','PositionsController');
+
 Route::resource('schools_colleges','SchoolsCollegesController');
 
 Route::resource('internal_trainings','InternalTrainingsController');
@@ -27,4 +29,6 @@ Route::resource('internal_trainings','InternalTrainingsController');
 Route::resource('external_trainings','ExternalTrainingsController');
 
 Route::resource('skills_competencies','SkillsCompetenciesController');
+
+
 
