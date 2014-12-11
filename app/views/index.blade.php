4436662eae1ddb2b037894e8a8d88d07db04a164
Ervy Yabut <ervy19@gmail.com>
2014-12-12 00:53:09 +0800

    Include login screen

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -1,9 +1,59 @@
-@extends('layouts.index')
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="assets/img/favicon.ico">
 
-@section('title')
-	Login
-@stop
+    <title>CEU HR TEAMS | Login </title>
 
-@section('content')
+    <!-- BEGIN GLOBAL MANDATORY STYLES -->
+    {{ HTML::style('assets/css/bootstrap.min.css'); }}
+    {{ HTML::style('assets/css/font-awesome.min.css'); }}
 
-@stop
+    <!-- BEGIN THEME STYLES -->
+    {{ HTML::style('assets/css/general-style.css'); }}
+    {{ HTML::style('assets/css/login-style.css'); }}
+
+  </head>
+
+  <body>
+
+	<div class="login">
+		<div class="row panel">
+	  		<form class="form-signin" role="form">
+	  			<div class="form-group">
+	  				<img src={{asset('assets/img/CEU_logo.jpg')}} alt="logo" class="img-responsive ceu-logo">
+		  			<h2>Human Resources</h2>
+		  			<h4>Training Evaluation and Monitoring System</h4>
+	  			</div>
+
+	  			<div class="input-group margin-bottom-sm">
+				  <span class="input-group-addon"><i class="fa fa-user fa-fw"></i></span>
+				  <input type="email" id="inputEmail" name="email" class="form-control" type="text" placeholder="Email Address">
+				</div>
+				<div class="input-group">
+				  <span class="input-group-addon"><i class="fa fa-lock fa-fw"></i></span>
+				  <input type="password" id="inputPassword" name="password" class="form-control" type="password" placeholder="Password">
+				</div>
+		        <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+		        <div class="checkbox">
+		          <label>
+		            <input type="checkbox" value="remember-me">Remember Me
+		          </label>
+		        </div>
+	      	</form>
+	  </div>
+	</div>
+  
+    <!-- BEGIN CORE JS -->
+
+    {{ HTML::script('https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'); }}
+
+    {{ HTML::script('assets/js/bootstrap.min.js'); }}
+
+  </body>
+</html>
