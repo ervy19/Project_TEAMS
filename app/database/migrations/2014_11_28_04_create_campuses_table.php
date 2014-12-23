ab1ce2f46e949255d4c638a5dbd348253849c4fa
Ervy Yabut <ervy19@gmail.com>
2014-12-23 11:35:28 +0800

    Fix issues with database migration files

--- app/database/migrations/2014_11_28_04_create_campuses_table.php
+++ app/database/migrations/2014_11_28_04_create_campuses_table.php
@@ -15,7 +15,7 @@ class CreateCampusesTable extends Migration {
 		Schema::create('campuses', function($table)
 		{
 			$table->increments('id');
-			$table->string('title', 100);
+			$table->string('name', 100);
 			$table->string('address', 255);
 
 			$table->boolean('isActive')->default(true);
