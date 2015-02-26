7cfc3dcfce8bcaef368eddd08ff744b93f6b9b56
John Donato <donatomato@gmail.com>
2015-02-26 20:47:23 +0800

    User Accounts Modal - INCOMPLETE

--- app/views/user_accounts/index.blade.php
+++ app/views/user_accounts/index.blade.php
@@ -11,7 +11,7 @@
 		<div class="row">
 		<div class="panel-heading">
 			<h1 class="panel-header">User Accounts</h1>
-			<button type="button" id="btn-add-sc" class="btn btn-primary pull-right" data-toggle="modal" data-target="#addSC">
+			<button type="button" id="btn-add-user-account" class="btn btn-primary pull-right" data-toggle="modal" data-target="#addUserAccount">
 				Add User Account<i class="fa fa-plus fa-lg add-plus"></i>
 			</button>
 		</div>
@@ -37,11 +37,47 @@
 	</div>
 </div>
 
+<!-- CREATE USER ACCOUNT MODAL-->
+<div class="modal fade" id="addUserAccount" tabindex="-1" role="dialog" aria-labelledby="addUserAccountLabel" aria-hidden="true" data-backdrop="static">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title" id="addUserAccountLabel"><i class="fa fa-building-o fa-lg"></i>&nbsp;&nbsp;Add User Account</h4>
+			</div>
+			<div class="modal-body">
+				<div class="col-sm-12 col-md-12">
+					<div class="row">
+						<!-- INSERT FORM -->
+						{{ Form::open(['data-add','id' => 'add-UserAccount', 'class' => 'form-horizontal']) }}
+						<div class="form-group row">
+							{{ Form::label('name', 'Name: ', array('class' => 'col-sm-1 col-md-1 control-label')) }}
+							<div class="col-sm-4 col-md-4">
+								{{ Form::text('name', '',array('class' => 'form-control')) }}
+								<div id="error-addUserAccount-name" class="error-message"></div>
+							</div>
+						</div>
+						<div class="form-group row">
+							{{ Form::label('type','Type: ', array('class' => 'col-sm-1 col-md-1 control-label')) }}
+							<div class="col-sm-4 col-md-4">
+								{{ Form::select('type', array( null =>  'Select Department Type', '0' => 'Non-Academic' , '1' => 'Academic'), null, array('id' => 'dd-departments-type', 'class' => 'form-control')) }}
+							<div id="error-adddepartment-type" class="error-message"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="modal-footer">
+					{{ Form::submit('Add User Account', array('class' => 'btn btn-primary')) }}
+				{{ Form::close() }}
+			</div>
+		</div>
+	</div>
+</div>
+
 @stop
 
 @section('page_js')
-	<script type="text/javascript">
-		
+	<script type="text/javascript">	
 		$(document).ready( function () {
 
 			var table = $('#tb-skills_competencies').dataTable({
